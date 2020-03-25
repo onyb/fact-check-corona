d1adcfa5bf5e237b15fb0900c5762b5e23705ad5
Anirudha Bose <anirudha.bose@ledger.fr>
2020-03-25 16:51:14 +0530

    Use app.logger for logging

--- api/app.py
+++ api/app.py
@@ -1,10 +1,6 @@
-import logging
-
 from flask import Flask, request
 from flask_cors import CORS
-
 from twilio.twiml.messaging_response import MessagingResponse
-
 
 app = Flask(__name__)
 CORS(app)
@@ -13,7 +9,7 @@ CORS(app)
 @app.route("/check", methods=["POST"])
 def check():
     data = request.get_json()
-    logging.info(data)
+    app.logger.info(data)
 
     resp = MessagingResponse()
     resp.message("Ahoy! Thanks so much for your message.")
