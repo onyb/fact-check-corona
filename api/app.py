95edbff7470addbb1dbe5fcab11330b29e87576a
Anirudha Bose <anirudha.bose@ledger.fr>
2020-03-25 16:04:05 +0530

    Basic Twilio integration

--- api/app.py
+++ api/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, jsonify, request
+import logging
+
+from flask import Flask, request
 from flask_cors import CORS
+from twilio import twiml
 
 app = Flask(__name__)
 CORS(app)
@@ -8,7 +11,12 @@ CORS(app)
 @app.route("/check", methods=["POST"])
 def check():
     data = request.get_json()
-    return jsonify({"content": data})
+    logging.info(data)
+
+    resp = twiml.Response()
+    resp.message("Ahoy! Thanks so much for your message.")
+
+    return str(resp)
 
 
 if __name__ == "__main__":
---
+++ api/database.py
@@ -0,0 +1,3 @@
+POSITIVELY_FAKE = [
+    "",
+]
